862b35794aeecf62b2bec97a7bc2c21de2a48c88
Arseniy <Bezruchkoarseniy@gmail.com>
2023-07-18 12:43:47 +1100

    Обновление install скрипта

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-require "application/lib/Dev.php";
-$config = require "application/config/config.php";
-
-use application\core\Router;
-
-spl_autoload_register(function ($class){
-  $path = str_replace("\\", "/", $class.".php");
-  if (file_exists($path)) {
-    include $path;
-  }
-});
-session_start();
-
-$router = new Router;
-$router->run();<?php
 ini_set('display_errors', '0');
 
 
